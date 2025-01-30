a4c2e4683f48fa9ffb1978e195f553f644a39809
Murugan Pachamalaiyan <71224378+mpachama@users.noreply.github.com>
2025-01-30 15:10:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Input(BaseModel):
+    text: str
 
 # Define the app
 app = FastAPI(
@@ -7,7 +11,12 @@ app = FastAPI(
     version="0.1.0"
 )
 
-#Define the APIs
+# Define a GET operation
 @app.get("/")
 async def main():
     return {"message": "Hello World"}
+
+# Define a POST operation
+@app.post("/submit")
+async def submit(input: Input):
+    return {"message": f"Data submitted is: {input.text}"}
